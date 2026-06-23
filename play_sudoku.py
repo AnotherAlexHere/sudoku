e532ea93d4726e509d35a59e036b9c36ff2cb65a
Agnieszka Palasz <agnieszka.k.palasz@gmail.com>
2026-06-23 18:09:30 +0100

    very basic user input, needs development

--- play_sudoku.py
+++ play_sudoku.py
@@ -2,3 +2,46 @@ from displaying_sudoku import *
 from fill_in_sudoku import *
 from is_sudoku_valid import *
 
+
+def simple_sudoku():
+    sudoku = [
+        [" "," "," ",2,6," ",7," ",1],
+        [6,8," "," ",7," "," ",9," "],
+        [1,9," "," "," ",4,5," "," "],
+        [8,2," ",1," "," "," ",4," "],
+        [" "," ",4,6," ",2,9," "," "],
+        [" ",5," "," "," ",3," ",2,8],
+        [" "," ",9,3," "," "," ",7,4],
+        [" ",4," "," ",5," "," ",3,6],
+        [7," ",3," ",1,8," "," "," "]
+    ]
+
+    completed_sudoku = [
+        [4,3,5,2,6,9,7,8,1],
+        [6,8,2,5,7,1,4,9,3],
+        [1,9,7,8,3,4,5,6,2],
+        [8,2,6,1,9,5,3,4,7],
+        [3,7,4,6,8,2,9,1,5],
+        [9,5,1,7,4,3,6,2,8],
+        [5,1,9,3,2,6,8,7,4],
+        [2,4,8,9,5,7,1,3,6],
+        [7,6,3,4,1,8,2,5,9]
+    ]
+    while True:
+        print_sudoku(sudoku)
+        user_input = input("Either enter break to stop the loop or enter the number \n you would like to input and the location in the format: number, row, column):")
+        
+        if user_input == "break":
+            break
+
+        user_input = user_input.split(", ")
+
+        add_entry(sudoku, int(user_input[0]), int(user_input[1]), int(user_input[2]))
+
+
+        if sudoku_complete(sudoku):
+            print_sudoku(sudoku)
+            break
+
+
+simple_sudoku()
